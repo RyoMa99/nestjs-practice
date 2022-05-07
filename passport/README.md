efc49124c21e7d349d479a3759ab327880ff94c0
rym49 <hatanakaryoma.130@gmail.com>
2022-05-07 20:38:25 +0900

    custom decoratorで認証なしルートを作る

--- passport/README.md
+++ passport/README.md
@@ -1,2 +1,3 @@
 # JWT認証
-[NestJSでJWTを使った認証を実装する](https://zenn.dev/uttk/articles/9095a28be1bf5d)
+- [NestJSでJWTを使った認証を実装する](https://zenn.dev/uttk/articles/9095a28be1bf5d)
+- [一部のルートだけ認証不要でアクセスさせたい](https://blog.n-t.jp/post/tech/nestjs-avoid-global-authguard-in-some-routes/)
--- passport/src/app.controller.ts
+++ passport/src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Public } from './decorator/public.decorator';
 
+@UseGuards(JwtAuthGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('jwt')
+  getHelloWithJwt(): string {
+    return this.appService.getHello();
+  }
+
+  @Public()
+  @Get('unjwt')
+  getHelloWithNoJwt(): string {
     return this.appService.getHello();
   }
 }
